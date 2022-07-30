d26b049c217b312d6481f3dfb4784fd53bf809f6
Sojin <voise.ksing@gmail.com>
2022-07-30 20:56:18 +0900

    22.07.30

---
+++ 2장/js/text-color.js
@@ -0,0 +1,6 @@
+//heading변수를 선언하고 h1태그를 저장한 꼴이 되었다.
+var heading = document.querySelector("#heading");
+//h1태그를 클릭하면 글자 색깔을 red로 설정하시오.
+heading.onclick =  function() {
+    heading.style.color = "red";
+}
